84d42324e91772de1e0b8d888f56b3a1a2650f1d
Hayden Muhl <haydenmuhl@gmail.com>
2024-11-30 22:23:15 -0500

    Project structure set up

--- src/day01.nim
+++ src/day01.nim
@@ -1,5 +1,9 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
+proc main =
+  echo("foo")
+
+
 when isMainModule:
-  echo("Hello, World!")
+  main()
