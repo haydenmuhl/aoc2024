6e5bd82bd2a4cee38863c96f5c5a7e9cea15b6b4
Hayden Muhl <haydenmuhl@gmail.com>
2024-12-02 00:08:41 -0500

    Day 1

--- src/day01.nim
+++ src/day01.nim
@@ -1,9 +1,56 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/sequtils
+import std/strutils
+import std/algorithm
+import std/tables
 
-proc main =
-  echo("foo")
+proc part1 =
+  let infile = open("data/day01.txt")
+  defer: infile.close()
+
+  var total = 0
+  var listA: seq[int] = newSeq[int]()
+  var listB: seq[int] = newSeq[int]()
+  for line in infile.lines:
+    let a = line[0 ..< 5].parseInt()
+    listA.add(a)
+    let b = line[8 ..< 13].parseInt()
+    listB.add(b)
+  listA.sort()
+  listB.sort()
+
+  while len(listA) > 0:
+    let a = listA.pop()
+    let b = listB.pop()
+    let diff = a - b
+    let sign = if diff >= 0: 1 else: -1
+    total += diff * sign
+  echo(total)
+
+
+proc part2 =
+  let infile = open("data/day01.txt")
+  defer: infile.close()
+
+  var total = 0
+  var listA: seq[int] = newSeq[int]()
+  var listB: seq[int] = newSeq[int]()
+  for line in infile.lines:
+    let a = line[0 ..< 5].parseInt()
+    listA.add(a)
+    let b = line[8 ..< 13].parseInt()
+    listB.add(b)
+
+  var histogram = initTable[int, int]()
+  for key in listA:
+    histogram[key] = 0
+  for key in listB:
+    if histogram.hasKey(key):
+      histogram[key] += 1
+
+  for key, value in histogram:
+    total += key * value
+  echo(total)
 
 
 when isMainModule:
-  main()
+  part2()
